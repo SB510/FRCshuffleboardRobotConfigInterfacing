09b20d8b9eda98da6eda473abcdd06c16d3b6a2b
SB510 <sean.borneman@gmail.com>
2022-02-22 18:50:32 -0500

    Interface works

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -16,53 +16,26 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.util.WPILibVersion;
 
 public class Robot extends TimedRobot {
-  private final DifferentialDrive m_tankDrive =
-      new DifferentialDrive(new PWMSparkMax(0), new PWMSparkMax(1));
-  private final Encoder m_leftEncoder = new Encoder(0, 1);
-  private final Encoder m_rightEncoder = new Encoder(2, 3);
-
-  private final PWMSparkMax m_elevatorMotor = new PWMSparkMax(2);
-  private final AnalogPotentiometer m_elevatorPot = new AnalogPotentiometer(0);
-  private NetworkTableEntry m_maxSpeed;
-
+  RobotConfig robotWork = new RobotConfig();
+  private long lastTime;
   @Override
   public void robotInit() {
-    System.out.println(RobotConfig.TEST_VALUE);
-    RobotConfig.getInstance();
-    System.out.println(RobotConfig.TEST_VALUE);
+
     // Add a 'max speed' widget to a tab named 'Configuration', using a number slider
-    // The widget will be placed in the second column and row and will be TWO columns wide
-    m_maxSpeed =
-        Shuffleboard.getTab("Configuration")
-            .add("Max Speed", 1)
-            .withWidget("Number Slider")
-            .withPosition(4, 1)
-            .withSize(2, 1)
-            .getEntry();
 
-    // Add the tank drive and encoders to a 'Drivebase' tab
-    ShuffleboardTab driveBaseTab = Shuffleboard.getTab("Drivebase");
-    driveBaseTab.add("Tank Drive", m_tankDrive);
-    // Put both encoders in a list layout
-    ShuffleboardLayout encoders =
-        driveBaseTab.getLayout("List Layout", "Encoders").withPosition(0, 0).withSize(2, 2);
-    encoders.add("Left Encoder", m_leftEncoder);
-    encoders.add("Right Encoder", m_rightEncoder);
-
-    // Add the elevator motor and potentiometer to an 'Elevator' tab
-    ShuffleboardTab elevatorTab = Shuffleboard.getTab("Elevator");
-    elevatorTab.add("Motor", m_elevatorMotor);
-    elevatorTab.add("Potentiometer", m_elevatorPot);
-    
+    robotWork.InitializeShuffleBoard();
     while (true){
-      System.out.println("Test Value:"+RobotConfig.getInstance().TEST_VALUE);
-      System.out.println("Test 2:"+RobotConfig.getInstance().Test_2);
+      if(System.currentTimeMillis()-lastTime > 1000){
+        robotWork.UpdateVariables();
+        //System.out.println(RobotConfig.driveTrain.autoCorrect);
+        lastTime = System.currentTimeMillis();
+      }
+      
     }
   }
 
   @Override
   public void autonomousInit() {
-    // Read the value of the 'max speed' widget from the dashboard
-    m_tankDrive.setMaxOutput(m_maxSpeed.getDouble(1.0));
+
   }
 }
